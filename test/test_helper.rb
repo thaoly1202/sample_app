7b3309c8f456452779fadcf3cbfc29aaa437513f
thaoly1202 <phamthithaoly1202@gmail.com>
2019-05-28 10:44:57 +0700

    Rails-flavored Ruby
    Chapter 5
    Filling in the layout

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,10 @@
+# frozen_string_literal: true
+
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
 
 class ActiveSupport::TestCase
   fixtures :all
+  include ApplicationHelper
 end
