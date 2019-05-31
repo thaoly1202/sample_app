33f5a25d42161265874e95d4ed35f7aa2e47e7b8
thaoly1202 <phamthithaoly1202@gmail.com>
2019-05-31 10:44:13 +0700

    Chapter 14: Following users

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,6 @@
 class Micropost < ApplicationRecord
   belongs_to :user
-  scope :lastest, ->{order created_at: :desc}
+  scope :order_created, ->{order created_at: :desc}
   mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :content, presence: true,
